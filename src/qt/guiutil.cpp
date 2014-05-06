db7d994a575d41b79074027d850d0881ac619299
barcoin-project <dev@barcoin.org>
2014-05-06 19:34:02 -0400

    Replaced smart quotes

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -88,7 +88,7 @@ void setupAmountWidget(QLineEdit *widget, QWidget *parent)
 bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
 {
     // return if URI is not valid or is no bitcoin URI
-    if(!uri.isValid() || uri.scheme() != QString(“barcoin"))
+    if(!uri.isValid() || uri.scheme() != QString("barcoin"))
         return false;
 
     SendCoinsRecipient rv;
@@ -143,9 +143,9 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
     //
     //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
     //    which will lower-case it (and thus invalidate the address).
-    if(uri.startsWith(“barcoin://"))
+    if(uri.startsWith("barcoin://"))
     {
-        uri.replace(0, 10, “barcoin:");
+        uri.replace(0, 10, "barcoin:");
     }
     QUrl uriInstance(uri);
     return parseBitcoinURI(uriInstance, out);
@@ -305,7 +305,7 @@ bool ToolTipToRichTextFilter::eventFilter(QObject *obj, QEvent *evt)
 #ifdef WIN32
 boost::filesystem::path static StartupShortcutPath()
 {
-    return GetSpecialFolderPath(CSIDL_STARTUP) / “Barcoin.lnk";
+    return GetSpecialFolderPath(CSIDL_STARTUP) / "Barcoin.lnk";
 }
 
 bool GetStartOnSystemStartup()
@@ -387,7 +387,7 @@ boost::filesystem::path static GetAutostartDir()
 
 boost::filesystem::path static GetAutostartFilePath()
 {
-    return GetAutostartDir() / “barcoin.desktop";
+    return GetAutostartDir() / "barcoin.desktop";
 }
 
 bool GetStartOnSystemStartup()
@@ -499,7 +499,7 @@ bool SetStartOnSystemStartup(bool fAutoStart) { return false; }
 HelpMessageBox::HelpMessageBox(QWidget *parent) :
     QMessageBox(parent)
 {
-    header = tr(“Barcoin-Qt") + " " + tr("version") + " " +
+    header = tr("Barcoin-Qt") + " " + tr("version") + " " +
         QString::fromStdString(FormatFullVersion()) + "\n\n" +
         tr("Usage:") + "\n" +
         "  barcoin-qt [" + tr("command-line options") + "]                     " + "\n";
@@ -511,7 +511,7 @@ HelpMessageBox::HelpMessageBox(QWidget *parent) :
         "  -min                   " + tr("Start minimized") + "\n" +
         "  -splash                " + tr("Show splash screen on startup (default: 1)") + "\n";
 
-    setWindowTitle(tr(“Barcoin-Qt"));
+    setWindowTitle(tr("Barcoin-Qt"));
     setTextFormat(Qt::PlainText);
     // setMinimumWidth is ignored for QMessageBox so put in non-breaking spaces to make it wider.
     setText(header + QString(QChar(0x2003)).repeated(50));
