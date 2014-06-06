6da9f4469b654140a3e49ba78a9f8af16021ac62
barcoin-project <dev@barcoin.org>
2014-06-06 05:55:06 -0400

    Reduce block time to 2 minutes

--- src/main.cpp
+++ src/main.cpp
@@ -1093,7 +1093,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 }
 
 static const int64 nTargetTimespan = 3.5 * 24 * 60 * 60; // Litecoin: 3.5 days
-static const int64 nTargetSpacing = 2.5 * 60; // Litecoin: 2.5 minutes
+static const int64 nTargetSpacing = 2.0 * 60; // Barcoin: 2.0 minutes
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 //
